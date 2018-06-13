32743f0f04ffd0de0aec6b32af459752b6c1c8db
Danny Ryan <dannyjryan@gmail.com>
2018-06-13 10:22:27 -0500

    port from eth research. organize into directories

---
+++ beacon_chain/bls.py
@@ -0,0 +1,95 @@
+try:
+    from hashlib import blake2s
+except:
+    from pyblake2 import blake2s
+blake = lambda x: blake2s(x).digest()
+from py_ecc.optimized_bn128 import G1, G2, neg, add, multiply, FQ, FQ2, FQ12, pairing, \
+    normalize, field_modulus, b, b2, is_on_curve, curve_order, final_exponentiate
+
+def compress_G1(pt):
+    x, y = normalize(pt)
+    return x.n + 2**255 * (y.n % 2)
+
+def decompress_G1(p):
+    if p == 0:
+        return (FQ(1), FQ(1), FQ(0))
+    x = p % 2**255
+    y_mod_2 = p // 2**255
+    y = pow((x**3 + b.n) % field_modulus, (field_modulus+1)//4, field_modulus)
+    assert pow(y, 2, field_modulus) == (x**3 + b.n) % field_modulus
+    if y%2 != y_mod_2:
+        y = field_modulus - y
+    return (FQ(x), FQ(y), FQ(1))
+
+# 16th root of unity
+hex_root = FQ2([21573744529824266246521972077326577680729363968861965890554801909984373949499,
+                16854739155576650954933913186877292401521110422362946064090026408937773542853])
+
+assert hex_root ** 8 != FQ2([1,0])
+assert hex_root ** 16 == FQ2([1,0])
+
+def sqrt_fq2(x):
+    y = x ** ((field_modulus ** 2 + 15) // 32)
+    while y**2 != x:
+        y *= hex_root
+    return y
+
+cache = {}
+
+def hash_to_G2(m):
+    if m in cache:
+        return cache[m]
+    k2 = m
+    while 1:
+        k1 = blake(k2)
+        k2 = blake(k1)
+        x1 = int.from_bytes(k1, 'big') % field_modulus
+        x2 = int.from_bytes(k2, 'big') % field_modulus
+        x = FQ2([x1, x2])
+        xcb = x**3 + b2
+        if xcb ** ((field_modulus ** 2 - 1) // 2) == FQ2([1,0]):
+            break
+    y = sqrt_fq2(xcb)
+    o = multiply((x, y, FQ2([1,0])), 2*field_modulus-curve_order)
+    cache[m] = o
+    return o
+
+def compress_G2(pt):
+    assert is_on_curve(pt, b2)
+    x, y = normalize(pt)
+    return (x.coeffs[0] + 2**255 * (y.coeffs[0] % 2), x.coeffs[1])
+
+def decompress_G2(p):
+    x1 = p[0] % 2**255
+    y1_mod_2 = p[0] // 2**255
+    x2 = p[1]
+    x = FQ2([x1, x2])
+    if x == FQ2([0, 0]):
+        return FQ2([1,0]), FQ2([1,0]), FQ2([0,0])
+    y = sqrt_fq2(x**3 + b2)
+    if y.coeffs[0] % 2 != y1_mod_2:
+        y = y * -1
+    assert is_on_curve((x, y, FQ2([1,0])), b2)
+    return x, y, FQ2([1,0])
+
+def sign(m, k):
+    return compress_G2(multiply(hash_to_G2(m), k))
+
+def privtopub(k):
+    return compress_G1(multiply(G1, k))
+
+def verify(m, pub, sig):
+    return final_exponentiate(pairing(decompress_G2(sig), G1, False) * \
+                              pairing(hash_to_G2(m), neg(decompress_G1(pub)), False)) == FQ12.one()
+
+def aggregate_sigs(sigs):
+    o = FQ2([1,0]), FQ2([1,0]), FQ2([0,0])
+    for s in sigs:
+        o = add(o, decompress_G2(s))
+    return compress_G2(o)
+
+def aggregate_pubs(pubs):
+    o = FQ(1), FQ(1), FQ(0)
+    for p in pubs:
+        o = add(o, decompress_G1(p))
+    return compress_G1(o)
---
+++ test/test_full_pos.py
@@ -0,0 +1,97 @@
+from full_pos import blake, mk_genesis_state_and_block, compute_state_transition, \
+    get_attesters_and_signer, Block, get_checkpoint_aggvote_msg, AggregateVote, \
+    SHARD_COUNT, ATTESTER_COUNT, get_shard_attesters
+import random
+import bls
+from simpleserialize import serialize, deserialize, eq, deepcopy
+import time
+
+privkeys = [int.from_bytes(blake(str(i).encode('utf-8'))[:4], 'big') for i in range(1000)]
+print('Generated privkeys')
+keymap = {}
+for i,k in enumerate(privkeys):
+    keymap[bls.privtopub(k)] = k
+    if i%50 == 0:
+        print("Generated %d keys" % i)
+
+def mock_make_child(parent_state, parent, skips, attester_share=0.8, checkpoint_shards=[]):
+    crystallized_state, active_state = parent_state
+    parent_attestation_hash = parent.hash
+    validator_count = len(crystallized_state.active_validators)
+    indices, main_signer = get_attesters_and_signer(crystallized_state, active_state, skips)
+    print('Selected indices: %r' % indices)
+    print('Selected main signer: %d' % main_signer)
+    # Randomly pick indices to include
+    bitfield = [1 if random.random() < attester_share else 0 for i in indices]
+    # Attestations
+    sigs = [bls.sign(parent_attestation_hash, keymap[crystallized_state.active_validators[indices[i]].pubkey])
+            for i in range(len(indices)) if bitfield[i]]
+    attestation_aggregate_sig = bls.aggregate_sigs(sigs)
+    print('Aggregated sig')
+    attestation_bitmask = bytearray((len(bitfield)-1) // 8 + 1)
+    for i, b in enumerate(bitfield):
+        attestation_bitmask[i//8] ^= (128 >> (i % 8)) * b
+    print('Aggregate bitmask:', bin(int.from_bytes(attestation_bitmask, 'big')))
+    # Randomly pick indices to include for checkpoints
+    shard_aggregate_votes = []
+    for shard, crosslinker_share in checkpoint_shards:
+        print('Making crosslink in shard %d' % shard)
+        indices = get_shard_attesters(crystallized_state, shard)
+        print('Indices: %r' % indices)
+        bitfield = [1 if random.random() < crosslinker_share else 0 for i in indices]
+        bitmask = bytearray((len(bitfield)+7) // 8)
+        for i, b in enumerate(bitfield):
+            bitmask[i//8] ^= (128 >> (i % 8)) * b
+        print('Bitmask:', bin(int.from_bytes(bitmask, 'big')))
+        checkpoint = blake(bytes([shard]))
+        checkpoint_attestation_hash = get_checkpoint_aggvote_msg(shard, checkpoint, crystallized_state)
+        sigs = [bls.sign(checkpoint_attestation_hash, keymap[crystallized_state.active_validators[indices[i]].pubkey])
+                for i in range(len(indices)) if bitfield[i]]
+        v = AggregateVote(shard_id=shard,
+                          checkpoint_hash=checkpoint,
+                          signer_bitmask=bitmask,
+                          aggregate_sig=list(bls.aggregate_sigs(sigs)))
+        shard_aggregate_votes.append(v)
+    print('Added %d shard aggregate votes' % len(checkpoint_shards))
+    # State calculations
+    o = Block(parent_hash=parent.hash,
+              skip_count=skips,
+              randao_reveal=blake(str(random.random()).encode('utf-8')),
+              attestation_bitmask=attestation_bitmask,
+              attestation_aggregate_sig=list(attestation_aggregate_sig),
+              shard_aggregate_votes=shard_aggregate_votes,
+              main_chain_ref=b'\x00'*32,
+              state_hash=b'\x00'*64)
+    print('Generated preliminary block header')
+    new_crystallized_state, new_active_state = \
+        compute_state_transition((crystallized_state, active_state), parent, o, verify_sig=False)
+    print('Calculated state transition')
+    if crystallized_state == new_crystallized_state:
+        o.state_hash = blake(parent.state_hash[:32] + blake(serialize(new_active_state)))
+    else:
+        o.state_hash = blake(blake(serialize(new_crystallized_state)) + blake(serialize(new_active_state)))
+    # Main signature
+    o.sign(keymap[crystallized_state.active_validators[main_signer].pubkey])
+    print('Signed')
+    return o, new_crystallized_state, new_active_state
+
+c, a, block = mk_genesis_state_and_block(keymap.keys())
+print('Generated genesis state')
+print('Crystallized state length:', len(serialize(c)))
+print('Active state length:', len(serialize(a)))
+print('Block size:', len(serialize(block)))
+block2, c2, a2 = mock_make_child((c, a), block, 0, 0.8, [])
+t = time.time()
+assert compute_state_transition((c, a), block, block2)
+print("Normal block (basic attestation only) processed in %.4f sec" % (time.time() - t))
+print('Verified a block!')
+block3, c3, a3 = mock_make_child((c2, a2), block2, 0, 0.8, [(0, 0.75)])
+print('Verified a block with a committee!')
+while a3.height % SHARD_COUNT > 0:
+    block3, c3, a3 = mock_make_child((c3, a3), block3, 0, 0.8, [(a3.height, 0.6 + 0.02 * a3.height)])
+    print('Height: %d' % a3.height)
+print('FFG bitmask:', bin(int.from_bytes(a3.ffg_voter_bitmask, 'big')))
+block4, c4, a4 = mock_make_child((c3, a3), block3, 1, 0.55, [])
+t = time.time()
+assert compute_state_transition((c3, a3), block3, block4)
+print("Epoch transition processed in %.4f sec" % (time.time() - t))
