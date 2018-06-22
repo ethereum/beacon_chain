4f24026f6870a8951c9a2311b27e9ee9272648c2
Chih Cheng Liang <chihchengliang@gmail.com>
2018-06-22 23:12:06 +0800

    fix test_no_reuse_of_pubkey comment

--- tests/contracts/test_deposit.py
+++ tests/contracts/test_deposit.py
@@ -28,15 +28,15 @@ def test_deposit(registration_contract, a0, w3, success, amount_deposit, assert_
 def test_no_reuse_of_pubkey(registration_contract, a0, w3, assert_tx_failed):
 
     call = registration_contract.functions.deposit(
-        b'\x00'*32,
+        b'\x55'*32,
         43,
         a0,
         b'\x00'*32)
 
-    # Register a0 once.
+    # Register pubkey b'\x55'*32 once.
     assert call.transact({"value": w3.toWei(32, "ether")})
 
-    # Register a0 twice would fail
+    # Register pubkey b'\x55'*32 twice would fail
     assert_tx_failed(
         lambda: call.transact({"value": w3.toWei(32, "ether")})
     )
