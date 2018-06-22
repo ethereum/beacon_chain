0d9bf9c1aa70c042309e33b9f8138748e48c15d5
Chih Cheng Liang <chihchengliang@gmail.com>
2018-06-22 17:31:32 +0800

    test no reuse of public key

--- tests/contracts/test_deposit.py
+++ tests/contracts/test_deposit.py
@@ -19,3 +19,16 @@ def test_deposit(registration_contract, a0, w3, success, amount_deposit, assert_
         assert_tx_failed(
             lambda: call.transact({"value": w3.toWei(amount_deposit, "ether")})
         )
+
+
+def test_no_reuse_of_pubkey(registration_contract, a0, w3, assert_tx_failed):
+
+    call = registration_contract.functions.deposit(b'\x00'*32, 43, a0, b'\x00'*32)
+
+    # Register a0 once.
+    assert call.transact({"value": w3.toWei(32, "ether")})
+
+    # Register a0 twice would fail
+    assert_tx_failed(
+        lambda: call.transact({"value": w3.toWei(32, "ether")})
+    )
