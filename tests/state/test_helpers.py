3147615a664c25d0310e5159df83ab352864cab6
Jannik Luhn <jannik.luhn@brainbot.com>
2018-07-02 12:09:11 +0200

    Add test helpers

--- tests/state/test_helpers.py
+++ tests/state/test_helpers.py
@@ -1,15 +1,33 @@
 import pytest
 
+import secrets
+
+from beacon_chain.state.config import (
+    DEFAULT_CONFIG,
+)
+from beacon_chain.state.validator_record import (
+    ValidatorRecord,
+)
 from beacon_chain.state.helpers import (
+    get_attesters_and_proposer,
     get_crosslink_shards,
     get_crosslink_notaries,
     get_crosslink_shards_count,
+    get_shuffling,
 )
 
 from tests.state.helpers import (
     mock_crystallized_state,
     mock_validator_record,
 )
+
+def test_get_shuffling():
+    assert get_shuffling(b"\x00", 0) == []
+    assert get_shuffling(b"\x00", 1) == [0]
+    assert get_shuffling(b"\x03", 1) == [0]
+    assert get_shuffling(b"\x00", 2) == [0, 1]
+    assert get_shuffling(b"\x03", 2) == [1, 0]
+
 
 @pytest.mark.parametrize(
     'active_validators_count, shard_count, notaries_per_crosslink, expected',
@@ -107,3 +125,47 @@ def test_get_crosslink_shards_error(genesis_crystallized_state,
     )
     with pytest.raises(exception):
         get_crosslink_shards(crystallized_state, config=config)
+
+
+@pytest.mark.skip
+def test_get_attesters_and_proposer(genesis_crystallized_state,
+                                    genesis_active_state):
+    crystallized_state = genesis_crystallized_state
+    active_state = genesis_active_state
+
+    original_attesters, original_proposer = get_attesters_and_proposer(
+        crystallized_state,
+        active_state,
+        skip_count=0,
+    )
+    assert 0 <= original_proposer < len(crystallized_state.active_validators)
+    assert all(0 <= i < len(crystallized_state.active_validators) for i in original_attesters)
+    assert len(original_attesters) == DEFAULT_CONFIG['attester_count']
+
+    skip_attesters, skip_proposer = get_attesters_and_proposer(
+        genesis_crystallized_state,
+        genesis_active_state,
+        skip_count=1,
+    )
+    assert skip_attesters == original_attesters
+    assert skip_proposer != original_proposer
+
+    active_state.randao = secrets.token_bytes(32)
+    reshuffled_attesters, reshuffled_proposer = get_attesters_and_proposer(
+        crystallized_state,
+        active_state,
+        skip_count=0
+    )
+    assert reshuffled_attesters != original_attesters
+    assert reshuffled_proposer != original_proposer
+
+    crystallized_state.active_validators = crystallized_state.active_validators[:3]
+    assert len(crystallized_state.active_validators) == 3
+    few_attesters, few_proposer = get_attesters_and_proposer(
+        crystallized_state,
+        active_state,
+        skip_count=0
+    )
+    assert len(few_attesters) == 3
+    assert set(few_attesters) == {0, 1, 2}
+    assert 0 <= few_proposer < 3
