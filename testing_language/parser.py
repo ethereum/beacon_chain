67f1fa941f525d2d3a7ba255251751962e0c0e01
Chih Cheng Liang <chihchengliang@gmail.com>
2018-08-24 17:48:15 +0800

    a text interpreter

--- testing_language/parser.py
+++ testing_language/parser.py
@@ -1,22 +1,67 @@
+import re
 
 
+class TestCase:
+    def __init__(self, sentence):
+        arrange_sentence, act_sentence, assert_sentence = sentence.split(':')
+        self.configs = {token[-1]: int(token[:-1])
+                        for token in arrange_sentence.split(',')}
+        self.actions = [token.strip() for token in act_sentence.split(',')]
+        self.assertions = list(assert_sentence.strip())
+
+    def arrange_test(self):
+        raise NotImplementedError
+
+    def act_test(self):
+        raise NotImplementedError
+
+    def assert_test(self):
+        raise NotImplementedError
+
+    def run(self):
+        self.arrange_test()
+        self.act_test()
+        self.assert_test()
 
 
-def parser_raw(sentence):
-    arrange_sentence, act_sentence, assert_sentence = sentence.split(':')
-    print(parse_arrange(arrange_sentence))
-    parse_act(act_sentence)
-    print(parse_assert(assert_sentence))
+class TextInterpreter(TestCase):
+    def arrange_test(self):
+        print("\nArrangments:\n")
+        print(f"First validator set: {self.configs['V']} validators")
+        print(f"EPOCH_LENGTH: {self.configs['E']}")
+        print(f"SHARD_COUNT: {self.configs['S']}")
+        print(f"MIN_COMMITTEE_SIZE: {self.configs['M']}")
 
-def parse_arrange(sentence):
-    tokens = sentence.split(',')
-    config = { token[-1]:int(token[:-1]) for token in tokens}
-    return config
+    def act_test(self):
+        self.slot = 1
+        print("\nActions:\n")
+        for action in self.actions:
+            print(f"Slot {self.slot}: ")
+            self.handle_action(action)
+            self.slot += 1
 
-def parse_act(sentence):
-    tokens = sentence.split(',')
-    for token in tokens:
-        print(token)
+    def handle_action(self, action):
+        if action == "":
+            print("\tDo nothing in this round")
+        else:
+            for token in action.split(" "):
+                print("\t", end="")
+                self.handle_action_token(token)
 
-def parse_assert(sentence):
-    return list(sentence.strip())
+    def handle_action_token(self, token):
+        if token.endswith(']'):
+            print(f"Assert: current slot {self.slot} should be {token[2:-1]}")
+        else:
+            regex = r'(?:\[([0-9]*)\])?([0-9]*)-([0-9]*)([A-Z\*])([A-Z\*])'
+            _slot, v_from, v_to, b_parent, b_vote = re.match(
+                regex, token).groups()
+            slot = self.slot if _slot is None else _slot
+            print(f"Validators {v_from}-{v_to} of the validator set of slot {slot}"
+                  f" vote on {b_vote}, a child of {b_parent}")
+
+    def assert_test(self):
+        print("\nAssertions:\n")
+        head, justified, finalized = self.assertions
+        print(f"Assert head is at {head}")
+        print(f"Assert last justified block is {justified}")
+        print(f"Assert last finalized block is {finalized}")
--- testing_language/test_parser.py
+++ testing_language/test_parser.py
@@ -1,9 +1,9 @@
-from parser import parser_raw
+from parser import TextInterpreter
 
 
 def test_parser():
     test_case = "100V,8E,32S,8M: 0-5*A, 0-5AB, 2-7AC [2]6-7AC, 0-4CD,, [=6] 0-4DE 5-7AB: E**"
-    parser_raw(test_case)
+    TextInterpreter(test_case).run()
 
 
 if __name__ == '__main__':
