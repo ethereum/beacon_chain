2da643eef5a2ed6d784c6d294abd61c724082c20
Danny Ryan <dannyjryan@gmail.com>
2018-09-23 08:08:36 -0500

    [rewards-penalties] use eth_utils.denoms for eth_to_wei

--- beacon_chain/state/constants.py
+++ beacon_chain/state/constants.py
@@ -1,7 +1,9 @@
+from eth_utils import denoms
+
 from beacon_chain.beacon_typing.custom import (
     Hash32,
 )
 
 
-WEI_PER_ETH = 10**18
+WEI_PER_ETH = denoms.ether
 ZERO_HASH32 = Hash32(32 * b'\x00')
